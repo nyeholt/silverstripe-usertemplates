1aa224256d6c6eec4cdefaf091f6b0662769d947
Marcus Nyeholt <marcus@symbiote.com.au>
2021-07-16 15:10:34 +1000

    chore(phpcs) Added phpcs fixes

--- src/UserTemplateAdmin.php
+++ src/UserTemplateAdmin.php
@@ -11,9 +11,10 @@ use SilverStripe\Admin\ModelAdmin;
  * @author marcus@silverstripe.com.au
  * @license BSD License http://silverstripe.org/bsd-license/
  */
-class UserTemplateAdmin extends ModelAdmin {
-	private static $menu_title = 'Templates';
-	private static $url_segment = 'templates';
-	private static $managed_models = array(UserTemplate::class);
+class UserTemplateAdmin extends ModelAdmin
+{
+    private static $menu_title = 'Templates';
+    private static $url_segment = 'templates';
+    private static $managed_models = array(UserTemplate::class);
     private static $menu_icon = 'symbiote/silverstripe-usertemplates: client/images/icons8-template-20.png';
 }
